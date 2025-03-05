353519d1a6efd2e58c473f8d99660e57d845422b
akshayyadav63 <akshay.kumar_cs22@gla.ac.in>
2025-03-05 23:22:38 +0530

    setup change flask to node.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,26 +2,24 @@
 // App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import 'font-awesome/css/font-awesome.min.css';
+import "font-awesome/css/font-awesome.min.css";
 import { UserProvider } from "./Components/UserContext";
 
+// Importing components with corrected paths
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Signup from "./Components/Signup";
 import Login from "./Components/Login";
 import AboutUs from "./Components/AboutUs";
-import AcademicAssessmentForm from './Components/Form/form'; // Correct path // Ensure the path is correct
-import Form from './Components/Form/form'
-import Quiz from './components/career/careerQuestion'
+import AcademicAssessmentForm from "./Components/Form/form"; // Corrected import
+import Quiz from "./Components/career/careerQuestion"; // Fixed case issue
 import Services from "./Components/Service";
 import ContactUs from "./Components/ContactUs";
-import CareerCounseling from "./Components/AssesmentHome";
-import SkillFront from "./Components/SkillFront";
-import SkillQuestions from "./Components/career/skillQuestions";
+import CareerCounseling from "./Components/career/AssesmentHome"; // Fixed path
+import SkillFront from "./Components/career/SkillFront"; // Fixed path
+import SkillQuestions from "./Components/career/skillQuestions"; // Fixed path
 import CareerDashboard from "./Components/CareerDasboar";
 import ResourceLibrary from "./Components/Resourse";
-
-
 
 function App() {
   return (
@@ -30,19 +28,18 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services/>} />
+          <Route path="/services" element={<Services />} />
           <Route path="/about" element={<AboutUs />} />
-          <Route path="/contact" element={<ContactUs/>} />
+          <Route path="/contact" element={<ContactUs />} />
           <Route path="/form" element={<AcademicAssessmentForm />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/quiz" element={< Quiz />} />
+          <Route path="/quiz" element={<Quiz />} />
           <Route path="/assesment-home" element={<CareerCounseling />} />
-          <Route path="/skillfront" element={<SkillFront/>} />
+          <Route path="/skillfront" element={<SkillFront />} />
           <Route path="/skillquestion" element={<SkillQuestions />} />
           <Route path="/dashboard" element={<CareerDashboard />} />
           <Route path="/resource" element={<ResourceLibrary />} />
-
         </Routes>
       </Router>
     </UserProvider>
